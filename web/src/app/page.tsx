3d9f881dfe300a627a90a67faedcfb11a531e795
Kauacnok <kauaprofessionalcontact@gmail.com>
2023-04-28 21:15:46 -0300

    done initial setup

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center w-screen h-screen">
+      <section className="w-4/5 h-4/5 bg-gray-50 rounded shadow"></section>
+    </main>
+  )
+}
